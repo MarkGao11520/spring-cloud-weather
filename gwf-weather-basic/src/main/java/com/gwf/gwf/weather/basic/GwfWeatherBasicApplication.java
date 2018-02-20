4d59232c844cfaacefbc5af6142deb7084f9ea5f
MarkGao11520 <1152057576@qq.com>
2018-02-20 22:13:24 +0800

    创建basic模块

---
+++ gwf-weather-basic/src/main/java/com/gwf/gwf/weather/basic/GwfWeatherBasicApplication.java
@@ -0,0 +1,12 @@
+package com.gwf.gwf.weather.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwfWeatherBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwfWeatherBasicApplication.class, args);
+	}
+}
