3c36d25a7bdabcdb8af692d47d499f6d7e6045dc
MarkGao11520 <1152057576@qq.com>
2018-02-21 15:15:19 +0800

    初始化数据采集服务

---
+++ msa-weather-collection-weather/src/main/java/com/gwf/weather/collection/MsaWeatherCollectionWeatherApplication.java
@@ -0,0 +1,12 @@
+package com.gwf.weather.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaWeatherCollectionWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaWeatherCollectionWeatherApplication.class, args);
+	}
+}
